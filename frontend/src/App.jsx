13211f6c7fe087c95f93040e1a1e5c2248cf4763
guntukarishika <guntukarishika@gmail.com>
2026-08-10 12:12:14 +0530

    Add investment page routing and navigation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,6 +10,7 @@ import IncomePage from './pages/IncomePage'
 import ExpensePage from './pages/ExpensePage'
 import BudgetPage from './pages/BudgetPage'
 import BillReminderPage from './pages/BillReminderPage'
+import InvestmentPage from './pages/InvestmentPage'
 import GoalsPage from './pages/GoalsPage'
 import NotFoundPage from './pages/NotFoundPage'
 
@@ -58,6 +59,14 @@ function App() {
           element={
             <ProtectedRoute>
               <BillReminderPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/investments"
+          element={
+            <ProtectedRoute>
+              <InvestmentPage />
             </ProtectedRoute>
           }
         />
